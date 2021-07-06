2e5e27a649dd70a1acdcc8fd16569ae89973deb1
melvinjoseph303 <82402414+melvinjoseph303@users.noreply.github.com>
2021-07-06 17:46:41 +0530

    Add files via upload

--- Frontend/src/app/newbook/newbook.component.ts
+++ Frontend/src/app/newbook/newbook.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { IBook } from '../bookmodel';
 import { BookService } from '../bookservice.service'
 import { Router } from '@angular/router';
@@ -9,13 +11,23 @@ import { Router } from '@angular/router';
 })
 export class NewbookComponent implements OnInit {
 
-  constructor(private bookService: BookService,private router: Router){  } 
+  constructor(private fb: FormBuilder,private bookService: BookService,private router: Router){  } 
   bookItem= {
     title:'',
     author:'',
     genre:'',
      image:''}
  // productItem: IProduct;
+
+ addbookForm = this.fb.group(
+  {
+    title: ['', Validators.required],
+    author: ['', Validators.required],
+    genre: ['', Validators.required],
+    image: ['', Validators.required]
+  }
+)
+
   ngOnInit() {
   }
   AddBook()
